154ff00b46f29a9691b59c92696616ae0f4b9a02
johnfa <john-f@online.no>
2014-02-06 20:06:40 +0100

    Creeated program to solve [12/11/13] Challenge #144 [Easy] Nuts & Bolts

---
+++ NutsAndBolts/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutsAndBolts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"C:\Users\johnfredrik\Documents\Visual Studio 2013\Projects\NutsAndBolts\NutsAndBolts\bin\Debug\input.txt");
+            var listLenght = int.Parse(lines[0]);
+            var list = new Dictionary<string, int>();        
+
+            for (int i = 1; i < lines.Length; i++)
+            {             
+                var splitt = lines[i].Split(' ');
+                var key = splitt[0];
+                var value = int.Parse(splitt[1]);
+
+                if (list.ContainsKey(key))
+                {
+                    var listValue = list[key];
+                    if ((value - listValue) != 0)
+                    {
+                        var stringValue = (value - listValue).ToString("+#;-#");
+                        Console.Write("{0} ",key);
+                        Console.Write(stringValue);
+                        Console.Write("\n");
+                    }
+                }
+                else
+                {
+                    list.Add(key,value);
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
